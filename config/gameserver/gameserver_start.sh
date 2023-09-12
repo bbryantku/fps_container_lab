a83497202a26107cf38db7c746964e3fb925140a
bbryantku <138116373+bbryantku@users.noreply.github.com>
2023-09-12 14:55:02 -0500

    Update gameserver_start.sh
    updated to change directory to /opt/gameserver prior to running gameserver binary

--- config/gameserver/gameserver_start.sh
+++ config/gameserver/gameserver_start.sh
@@ -5,6 +5,8 @@ ip route del default #removes docker default route
 ip route add default via 10.5.0.2 #adds vyos router IP as default route
 
 #switch to unprivileged account
-su testuser && /opt/gameserver/bin/Server.x86_64
+su testuser 
+cd /opt/gameserver
+/opt/gameserver/bin/Server.x86_64
 
 #start the indended service
