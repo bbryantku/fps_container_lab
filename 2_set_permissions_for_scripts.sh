5a2499a90f5697c6bf4cd00339b5cff4ec084e3a
bbryantku <138116373+bbryantku@users.noreply.github.com>
2023-09-14 12:11:22 -0500

    Delete 2_set_permissions_for_scripts.sh

--- 2_set_permissions_for_scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo chmod +x ./bin/gameclient/Headless_Client.x86_64
-sudo chmod +x ./config/gameclient01/gameclient_start.sh
-sudo chmod +x ./bin/gameserver/Server.x86_64
-sudo chmod +x ./config/gameserver/gameserver_start.sh
-sudo chown 0:0 ./config/gamerouter01/*
